d13846b25a5adb92eb693eb462e820b78730abac
Martin Konrad <konrad@frib.msu.edu>
2016-10-04 15:20:35 -0400

    Fix issues with handling of package_name

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,7 +4,7 @@ class epics_gateway::install(
   $package_name   = $epics_gateway::params::package_name,
   $package_ensure = $epics_gateway::params::package_ensure,
 ) {
-  validate_array($package_name)
+  validate_string($package_name)
   validate_string($package_ensure)
 
   package { 'procserv':
--- manifests/params.pp
+++ manifests/params.pp
@@ -24,7 +24,7 @@ class epics_gateway::params {
 
   case $::osfamily {
     'Debian': {
-      $package_name = [ 'epics-cagateway' ]
+      $package_name = 'epics-cagateway'
     }
     default: {
       fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
